1898f83838ab5b911d78fcb03b5c5ea74a010ee9
Théo <julientheo@eisti.eu>
2021-05-14 21:21:38 +0200

    second

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TextInput, Image } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+import logo from './images/logoAlpha.png';
+
+const { width: WIDTH } = Dimensions.get('window');
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient
+    colors={['#FF00FF', '#AA00FF']}
+    style={styles.backgroundContainer}>
+      <View style={styles.logoContainer}>
+        <Image source={logo} style={styles.logo}/>
+      </View>
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder={"Nom d'utilisateur"}
+          placeholderTextColor={'rgba(255,255,255,0.7)'}
+          underlineColorAndroid='transparent'/>
+      </View>
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder={"Nom d'utilisateur"}
+          placeholderTextColor={'rgba(255,255,255,0.7)'}
+          underlineColorAndroid='transparent'/>
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundContainer :{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: null,
+    height: null,
+    justifyContent: "center",
+    alignItems: "center"
   },
+  logo :{
+    width: 200,
+    height: 200
+  },
+  input :{
+    width: WIDTH - 55,
+    height: 45,
+    borderRadius: 45,
+    fontSize: 16,
+    paddingLeft: 45,
+    backgroundColor:'rgba(0,0,0,0.35)',
+    color:'rgba(255,255,255,0.7)',
+    marginHorizontal: 25,
+  },
+  inputIcon:{
+    position:'absolute',
+    top: 8,
+    left: 37
+  }
 });
