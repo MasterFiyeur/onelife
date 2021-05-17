154d05aad5de9d7bd0c81e147b88957603851b34
Théo <julientheo@eisti.eu>
2021-05-17 19:14:27 +0200

    register with scroll view

--- Login/Register.js
+++ Login/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Dimensions, TextInput, Image, TouchableOpacity, Keyboard } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TextInput, Image, TouchableOpacity, Keyboard,SafeAreaView,ScrollView } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/Ionicons';
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -7,6 +7,7 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import logo from '../images/logoAlpha.png';
 
 const { width: WIDTH } = Dimensions.get('window');
+const { height: HEIGHT } = Dimensions.get("window");
 
 export default class Register extends Component {
   constructor(props) {
@@ -78,66 +79,69 @@ export default class Register extends Component {
 
   render(){
   return (
+    <SafeAreaView style={styles.backgroundContainer}>
     <LinearGradient colors={['#FF00FF', '#AA00FF' ]} style={styles.backgroundContainer}>
       <View >
           <TouchableOpacity style={styles.backChevron} onPress={() => {this.props.navigation.navigate('login');}}>
             <Icon name='chevron-back' size={35} color='rgba(255,255,255,1)'></Icon>
           </TouchableOpacity>
       </View>
-      <View style={styles.backgroundCenter}>
-        <View style={styles.logoContainer}>
-          <Image source={logo} style={styles.logo} />
+      <ScrollView>
+        <View style={styles.backgroundCenter}>
+          <View style={styles.logoContainer}>
+            <Image source={logo} style={styles.logo} />
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput onChangeText={(text)=>{this.setState({username:text});}}
+              defaultValue={this.state.username}
+              style={[styles.input,!this.state.validMail?{borderColor: "red",borderWidth: 1}:""]}
+              placeholder={"Adresse mail"}
+              placeholderTextColor={'rgba(255,255,255,0.7)'}
+              underlineColorAndroid='transparent'/>
+              <Icon name='person-circle-outline' size={28} color='rgba(255,255,255,0.7)' style={styles.inputIcon}></Icon>
+          </View>
+          <TouchableOpacity style={{marginBottom:10}} onPress={()=>{this.setState({show:true})}}>
+              {this.state.show && <DateTimePicker
+                mode="date"
+                value={this.state.date}
+                minimumDate={new Date(1950,0,1)}
+                onChange={this.changeDate.bind(this)}
+                maximumDate={this.state.dateMax}
+              />}
+              <Text style={[styles.inputDate,,!this.state.validDate?{borderColor: "red",borderWidth: 1}:""]}>
+                {this.state.dateText}
+              </Text>
+          </TouchableOpacity>
+          <View style={styles.inputContainer}>
+            <TextInput onChangeText={(text)=>{this.setState({password:text});}}
+              defaultValue={this.state.password}
+              style={[styles.input,!this.state.validMDP?{borderColor: "red",borderWidth: 1}:""]}
+              placeholder={"Mot de passe (8 caractères)"}
+              secureTextEntry={this.state.showPass}
+              placeholderTextColor={'rgba(255,255,255,0.7)'}
+              underlineColorAndroid='transparent'/>
+              <Icon name='lock-closed' size={26} color='rgba(255,255,255,0.7)' style={styles.inputIcon}></Icon>
+              <TouchableOpacity style={styles.inputIcon2} onPress={this.showPass.bind(this)}>
+                <Icon name={this.state.press==false ? 'eye' : 'eye-off' } size={26} color='rgba(255,255,255,0.7)'></Icon>
+              </TouchableOpacity>
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput onChangeText={(text)=>{this.setState({passConf:text});}}
+              defaultValue={this.state.passConf}
+              style={[styles.input,!this.state.validMDP?{borderColor: "red",borderWidth: 1}:""]}
+              placeholder={"Confirmation mot de passe"}
+              secureTextEntry={this.state.showPass}
+              placeholderTextColor={'rgba(255,255,255,0.7)'}
+              underlineColorAndroid='transparent'/>
+              <Icon name='lock-closed' size={26} color='rgba(255,255,255,0.7)' style={styles.inputIcon}></Icon>
+          </View>
+          <TouchableOpacity style={styles.btnReg} onPress={this.registerVerif}>
+            <Text style={styles.text}>S'enregistrer</Text>
+          </TouchableOpacity>
         </View>
-        <View style={styles.inputContainer}>
-          <TextInput onChangeText={(text)=>{this.setState({username:text});}}
-            defaultValue={this.state.username}
-            style={[styles.input,!this.state.validMail?{borderColor: "red",borderWidth: 1}:""]}
-            placeholder={"Adresse mail"}
-            placeholderTextColor={'rgba(255,255,255,0.7)'}
-            underlineColorAndroid='transparent'/>
-            <Icon name='person-circle-outline' size={28} color='rgba(255,255,255,0.7)' style={styles.inputIcon}></Icon>
-        </View>
-        <TouchableOpacity style={{marginBottom:10}} onPress={()=>{this.setState({show:true})}}>
-            {this.state.show && <DateTimePicker
-              mode="date"
-              value={this.state.date}
-              minimumDate={new Date(1950,0,1)}
-              onChange={this.changeDate.bind(this)}
-              maximumDate={this.state.dateMax}
-            />}
-            <Text style={[styles.inputDate,,!this.state.validDate?{borderColor: "red",borderWidth: 1}:""]}>
-              {this.state.dateText}
-            </Text>
-        </TouchableOpacity>
-        <View style={styles.inputContainer}>
-          <TextInput onChangeText={(text)=>{this.setState({password:text});}}
-            defaultValue={this.state.password}
-            style={[styles.input,!this.state.validMDP?{borderColor: "red",borderWidth: 1}:""]}
-            placeholder={"Mot de passe (8 caractères)"}
-            secureTextEntry={this.state.showPass}
-            placeholderTextColor={'rgba(255,255,255,0.7)'}
-            underlineColorAndroid='transparent'/>
-            <Icon name='lock-closed' size={26} color='rgba(255,255,255,0.7)' style={styles.inputIcon}></Icon>
-            <TouchableOpacity style={styles.inputIcon2} onPress={this.showPass.bind(this)}>
-              <Icon name={this.state.press==false ? 'eye' : 'eye-off' } size={26} color='rgba(255,255,255,0.7)'></Icon>
-            </TouchableOpacity>
-        </View>
-        <View style={styles.inputContainer}>
-          <TextInput onChangeText={(text)=>{this.setState({passConf:text});}}
-            defaultValue={this.state.passConf}
-            style={[styles.input,!this.state.validMDP?{borderColor: "red",borderWidth: 1}:""]}
-            placeholder={"Confirmation mot de passe"}
-            secureTextEntry={this.state.showPass}
-            placeholderTextColor={'rgba(255,255,255,0.7)'}
-            underlineColorAndroid='transparent'/>
-            <Icon name='lock-closed' size={26} color='rgba(255,255,255,0.7)' style={styles.inputIcon}></Icon>
-        </View>
-        <TouchableOpacity style={styles.btnReg} onPress={this.registerVerif}>
-          <Text style={styles.text}>S'enregistrer</Text>
-        </TouchableOpacity>
-      </View>
-      <View></View>
+      </ScrollView>
     </LinearGradient>
+    </SafeAreaView>
   );}
 }
 
@@ -145,7 +149,7 @@ const styles = StyleSheet.create({
   backgroundContainer :{
     flex: 1,
     width: null,
-    height: null
+    height: null,
   },
   backgroundCenter :{
     flex: 1,
@@ -153,7 +157,7 @@ const styles = StyleSheet.create({
     height: null,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom:50
+    paddingTop: 25
   },
   backgroundTop :{
     flex: 1,
@@ -162,7 +166,7 @@ const styles = StyleSheet.create({
     flexDirection: "row"
   },
   backChevron:{
-    paddingTop: 15,
+    paddingVertical: 15,
     paddingLeft: 15,
     width: 50
   },
